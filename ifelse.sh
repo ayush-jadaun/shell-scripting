37ac1a0bc8487d0be956fa68c250b10c8e2d7430
ayush-jadaun <ayushjadaun6@gmail.com>
2025-08-10 05:22:02 +0000

    if else+elif

--- ifelse.sh
+++ ifelse.sh
@@ -8,8 +8,13 @@ read -p "Enter first number: " first
 read -p "Enter second number: " second
 
 if (( first > second )); then
-    echo "First is greater"
+
+    	echo "First is greater"
+
+elif ((first==second)); then
+	echo "Both are equal"	
 else
+
     echo "Second is greater"
 fi
 
