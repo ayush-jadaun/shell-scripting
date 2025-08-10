5d319aa6b9dadf7338313d09c622a1634c044274
ayush-jadaun <ayushjadaun6@gmail.com>
2025-08-10 05:00:26 +0000

    variables reading

---
+++ variables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#The above command is called shebang!!!!!!!!!!!!!!!!!!
+#
+<< comment
+Multiline comment
+hehe
+comment
+
+name="ayush"
+
+echo "Name is ${name}"
+
+# give file permissions, chmod 755 or 754 deepending on what you want
+
+echo "Enter another name"
+
+read -p "Enter name: " name
+
+echo "added name is ${name}"
+
+
+
+
+
